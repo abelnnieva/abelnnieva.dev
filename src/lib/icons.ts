c0aa065c48525c15dea672551cbda1de4785bbb0
Abel N. Nieva <931497+abelnnieva@users.noreply.github.com>
2024-06-26 18:48:46 +0100

    Refactor individual icon components into a single reusable Icon component

---
+++ src/lib/icons.ts
@@ -0,0 +1,52 @@
+interface Icon {
+  path: string;
+}
+
+interface Icons {
+  [key: string]: Icon;
+}
+
+const icons: Icons = {
+  link: {
+    path: `
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"
+      />
+    `,
+  },
+  "arrow-right": {
+    path: `
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+      />
+    `
+  },
+  menu: {
+    path: `
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+      />
+    `
+  },
+  hashtag: {
+    path: `
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5-3.9 19.5m-2.1-19.5-3.9 19.5"
+      />
+    `
+  }
+};
+
+const getIcon = (key: string): Icon => {
+  return icons[key];
+};
+
+export default getIcon;
