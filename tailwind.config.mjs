0562abb13a700f7dc65bd63c89dbc6888f046aca
Abel N. Nieva <931497+abelnnieva@users.noreply.github.com>
2024-07-15 23:37:49 +0100

    feat: Add PageHeader component and update Tailwind typography scale
    - Added a new PageHeader component to display the title and text with responsive styling.
    - Introduced the Container component for consistent layout structure in PageHeader.
    - Updated Tailwind configuration to adjust the typography scale for better text balance:
      - Increased line heights for xl, 2xl, 3xl, 4xl sizes.
      - Added a new 5xl size for larger headings.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -64,10 +64,11 @@ export default {
       sm: ["0.875rem", "1.25rem"], // 14px
       base: ["1rem", "1.5rem"], // 16px
       lg: ["1.125rem", "1.75rem"], // 18px
-      xl: ["1.25rem", "1.75rem"], // 20px
-      "2xl": ["1.375rem", "2rem"], // 22px
-      "3xl": ["1.75rem", "2.25rem"], // 28px
-      "4xl": ["2.25rem", "2.5rem"], // 36px
+      xl: ["1.25rem", "2rem"], // 20px
+      "2xl": ["1.375rem", "2.25rem"], // 22px
+      "3xl": ["1.5rem", "2.5rem"], // 24px
+      "4xl": ["1.75rem", "2.75rem"], // 28px
+      "5xl": ["2.25rem", "3rem"], // 36px
     },
   },
   plugins: [require("flowbite/plugin")],
